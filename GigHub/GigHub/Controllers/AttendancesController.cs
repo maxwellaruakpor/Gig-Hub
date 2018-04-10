249290d8b9ecc3d74f555ece4b999d8ddc7095c9
Maxwell Aruakpor <maxcom@live.com>
2018-04-09 21:43:21 -0400

    Add Web API

--- GigHub/GigHub/Controllers/AttendancesController.cs
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using GigHub.Models;
+﻿using GigHub.Dtos;
+using GigHub.Models;
 using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web.Http;
@@ -9,22 +10,24 @@ namespace GigHub.Controllers
     public class AttendancesController : ApiController
     {
         private ApplicationDbContext _context;
+
         public AttendancesController()
         {
-            _context = new ApplicationDbContext(); 
+            _context = new ApplicationDbContext();
+
         }
 
         [HttpPost]
-        public IHttpActionResult Attend([FromBody]int gigId)
+        public IHttpActionResult Attend(AttendanceDto dto)
         {
             var userId = User.Identity.GetUserId();
 
-            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId)) 
-                return BadRequest("The attendance already exist.");
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest("The attendance already exists.");
 
             var attendance = new Attendance
             {
-                GigId = gigId,
+                GigId = dto.GigId,
                 AttendeeId = userId
             };
             _context.Attendances.Add(attendance);
